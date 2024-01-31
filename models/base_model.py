ee6ee8264be94d14fc50d46e079646bef6862852
godwinsallah16 <godwinsallah16@gmail.com>
2024-01-31 15:32:18 +0000

    d

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,59 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
+"""Module base_model
+
+This Module contains a definition for BaseModel Class
+"""
+
+import uuid
 from datetime import datetime
+
+import models
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """BaseModel Class"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """__init__ method & instantiation of class Basemodel
 
         Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
+            *args.
+            **kwargs (dict): Key/value pairs
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if kwargs is not None and len(kwargs) > 0:
             for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
+                if k == "__class__":
+                    continue
+                elif k in ["created_at", "updated_at"]:
+                    setattr(self, k, datetime.fromisoformat(v))
                 else:
-                    self.__dict__[k] = v
+                    setattr(self, k, v)
         else:
             models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
+        """
+        bs_dict = (
+            {
+                k: (v.isoformat() if isinstance(v, datetime) else v)
+                for (k, v) in self.__dict__.items()
+            }
+        )
+        bs_dict["__class__"] = self.__class__.__name__
+        return bs_dict
 
-    def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+    def __str__(self) -> str:
+        """should print/str representation of the BaseModel instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
--- models/place.py
+++ models/place.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Defines the Place class."""
+"""Module base_model
+
+This Module contains a definition for Place Class
+"""
+
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Represent a place.
+    """A class that represents a place
 
     Attributes:
         city_id (str): The City id.
